609a2f8861545885ad1a40dbae9964c3920a3197
kminamiurawa24 <k.minami.urawa024@gmail.com>
2024-11-22 10:57:55 +0900

    API用のプロジェクトファイルを追加

---
+++ 02_KiracuFlyerAPI/KiracuFlyerAPI/Controllers/ConTestController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KiracuFlyerAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ConTestController : ControllerBase
+    {
+        /// <summary>
+        /// コネクションをテストする、GETなエンドポイント
+        /// エンドポイント/ConTest/ping
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("ping")]
+        public IActionResult Ping()
+        {
+            return Ok(new { message = "Pong!", timestamp = DateTime.UtcNow });
+        }
+    }
+}
