5e1606585d5cfd12e6fa6aa0878680aca3f018eb
kminamiurawa17 <k.minami.urawa017@gmail.com>
2024-12-04 10:40:25 +0900

    アプリのボタンを追加

--- 03_MobileApp/app/src/main/java/com/example/kiracuchat/UserOptionActivity.kt
+++ 03_MobileApp/app/src/main/java/com/example/kiracuchat/UserOptionActivity.kt
@@ -1,16 +1,19 @@
 package com.example.kiracuchat
-
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-import android.widget.ImageButton
 import androidx.activity.ComponentActivity
-import androidx.appcompat.app.AppCompatActivity
+
 
 
 class UserOptionActivity : ComponentActivity()  {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
             setContentView(R.layout.user_option)
+        val backBtn: Button = findViewById(R.id.back_btn)
+
+        backBtn.setOnClickListener {
+            startActivity(Intent(this,MainActivity::class.java))
+        }
     }
 }
