1333483b1974d490462b5dcafebf5a39c1af4ee8
kminamiurawa24 <k.minami.urawa024@gmail.com>
2024-12-24 11:46:59 +0900

    サービスに各メソッドを追加

--- 02_KiracuFlyerAPI/KiracuFlyerAPI/Service/UserService.cs
+++ 02_KiracuFlyerAPI/KiracuFlyerAPI/Service/UserService.cs
@@ -1,6 +1,38 @@
-﻿namespace KiracuFlyerAPI.Service
+﻿using KiracuFlyerAPI.Model;
+using KiracuFlyerAPI.Repository.Interface;
+
+namespace KiracuFlyerAPI.Service
 {
     public class UserService
     {
+        private readonly IUserRepository _userRepository;
+
+        public UserService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<User> GetByIdAsync(int id)
+        {
+            return await _userRepository.GetByIdAsync(id);
+        }
+
+        public async Task<User> CreateUserAsync(User user)
+        {
+            // バリデーション処理など
+            await _userRepository.AddAsync(user);
+            return user;
+        }
+
+        public async Task UpdateUserAsync(User user)
+        {
+            // バリデーション処理など
+            await _userRepository.UpdateAsync(user);
+        }
+
+        public async Task DeleteUserAsync(int id)
+        {
+            await _userRepository.DeleteAsync(id);
+        }
     }
 }
