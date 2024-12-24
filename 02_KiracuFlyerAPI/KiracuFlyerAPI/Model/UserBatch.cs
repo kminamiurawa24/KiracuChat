f9b263e0f79692cd0ec4d9f7bb400d9528d4ec09
kminamiurawa24 <k.minami.urawa024@gmail.com>
2024-12-24 10:22:55 +0900

    ユーザーバッチのモデルクラスを追加

---
+++ 02_KiracuFlyerAPI/KiracuFlyerAPI/Model/UserBatch.cs
@@ -0,0 +1,9 @@
+﻿namespace KiracuFlyerAPI.Model
+{
+    public class UserBatch
+    {
+        public int Id { get; set; }
+        public int Level { get; set; }
+        public string? ImageUrl { get; set; }
+    }
+}
