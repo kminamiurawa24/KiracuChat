3049d25988e5a1b1a14c6c4444f93c5d65b372fb
kminamiurawa24 <k.minami.urawa024@gmail.com>
2024-12-24 11:31:26 +0900

    リポジトリ、コンテキストを追加

---
+++ 02_KiracuFlyerAPI/KiracuFlyerAPI/DBContext/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using KiracuFlyerAPI.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace KiracuFlyerAPI.DBContext
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+    {
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ 02_KiracuFlyerAPI/KiracuFlyerAPI/Repository/Interface/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using KiracuFlyerAPI.Model;
+
+namespace KiracuFlyerAPI.Repository.Interface
+{
+    public interface IUserRepository
+    {
+        Task<User> GetByIdAsync(int id);
+        Task<User> GetByLoginIdAsync(string loginId);
+        Task<List<User>> GetAllAsync();
+        Task AddAsync(User user);
+        Task UpdateAsync(User user);
+        Task DeleteAsync(int id);
+    }
+}
