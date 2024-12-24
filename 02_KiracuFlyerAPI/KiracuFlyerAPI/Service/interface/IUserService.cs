0888c220802c20608c162ba4373f2053979d36e8
kminamiurawa24 <k.minami.urawa024@gmail.com>
2024-12-24 11:45:52 +0900

    各インターフェースを追加

--- 02_KiracuFlyerAPI/KiracuFlyerAPI/Service/interface/IUserService.cs
+++ 02_KiracuFlyerAPI/KiracuFlyerAPI/Service/interface/IUserService.cs
@@ -1,8 +1,13 @@
-﻿using KiracuFlyerAPI.Service;
+﻿using KiracuFlyerAPI.Model;
+using KiracuFlyerAPI.Service;
 
 namespace KiracuFlyerAPI.Service.Interface
 {
     public interface IUserService
     {
+        Task<User> GetByIdAsync(int id);
+        Task<User> CreateUserAsync(User user);
+        Task UpdateUserAsync(User user);
+        Task DeleteUserAsync(int id);
     }
 }
