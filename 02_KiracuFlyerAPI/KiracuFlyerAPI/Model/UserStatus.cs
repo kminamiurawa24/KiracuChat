02042dce42835dd561f448d3d593eecd8a87eaad
kminamiurawa24 <k.minami.urawa024@gmail.com>
2024-12-24 10:32:53 +0900

    ユーザーステータスモデルを追加

---
+++ 02_KiracuFlyerAPI/KiracuFlyerAPI/Model/UserStatus.cs
@@ -0,0 +1,11 @@
+﻿namespace KiracuFlyerAPI.Model
+{
+    public class UserStatus
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int StatusId { get; set; }
+        public string? CustomMessage { get; set; }
+        public DateTime? UpdateDate { get; set; }
+    }
+}
