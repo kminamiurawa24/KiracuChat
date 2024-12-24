bae6142990032c2d74fb6f1ad0e37ff747ba6854
kminamiurawa24 <k.minami.urawa024@gmail.com>
2024-12-24 10:35:08 +0900

    Iconのモデルクラスを追加

---
+++ 02_KiracuFlyerAPI/KiracuFlyerAPI/Model/UserIcon.cs
@@ -0,0 +1,9 @@
+﻿namespace KiracuFlyerAPI.Model
+{
+    public class UserIcon
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string? ImageUrl { get; set; }
+    }
+}
