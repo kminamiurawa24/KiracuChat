9979f7eb0ee0f6eb1028ef0af171a0c07bc0b706
kminamiurawa17 <k.minami.urawa017@gmail.com>
2024-12-25 14:46:56 +0900

    ログイン画面に遷移するようにした

---
+++ 03_MobileApp/app/src/main/java/com/example/kiracuchat/LogInActivity.kt
@@ -0,0 +1,12 @@
+package com.example.kiracuchat
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class LogInActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.log_in)
+
+    }
+}
--- 03_MobileApp/app/src/main/java/com/example/kiracuchat/MainActivity.kt
+++ 03_MobileApp/app/src/main/java/com/example/kiracuchat/MainActivity.kt
@@ -29,12 +29,16 @@ class MainActivity : ComponentActivity() {
 
         val userOptionBtn: Button = findViewById(R.id.user_option_btn)
         val channelBtn: Button = findViewById(R.id.channel_btn)
+        val logInBtn: Button = findViewById(R.id.Log_in_btn)
 
         userOptionBtn.setOnClickListener {
             startActivity(Intent(this, UserOptionActivity::class.java))
         }
         channelBtn.setOnClickListener {
             startActivity(Intent(this, channel::class.java))
+        }
+        logInBtn.setOnClickListener {
+            startActivity(Intent(this, LogInActivity::class.java))
         }
         CoroutineScope(Dispatchers.IO).launch {
             val result = fetchDataFromServer()
