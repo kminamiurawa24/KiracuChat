cf348d834e75fba6d3d913a3c78bba1ba3c7aeb2
kminamiurawa24 <k.minami.urawa024@gmail.com>
2024-12-26 13:26:12 +0900

    ステータスコントローラーをツイカ

---
+++ 02_KiracuFlyerAPI/KiracuFlyerAPI/Controllers/StatusController.cs
@@ -0,0 +1,27 @@
+﻿using KiracuFlyerAPI.Model;
+using KiracuFlyerAPI.Service.Interface;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KiracuFlyerAPI.Controllers
+{
+    [ApiController]
+    [Route("status")]
+    public class StatusController : ControllerBase
+    {
+        private readonly IStatusService _statusMasterService;
+
+        public StatusController(IStatusService statusMasterService)
+        {
+            _statusMasterService = statusMasterService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllStatuses()
+        {
+            // ステータス情報を全件取得
+            var statuses = await _statusMasterService.GetAllAsync();
+
+            return Ok(statuses);
+        }
+    }
+}
