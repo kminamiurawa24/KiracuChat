4e227c0f1d71551cedac909b0f9b23af07f69ed3
kminamiurawa24 <k.minami.urawa024@gmail.com>
2024-12-26 13:00:57 +0900

    ステータスマスタの空ファイルを追加

---
+++ 02_KiracuFlyerAPI/KiracuFlyerAPI/Repository/StatusMasterRepository.cs
@@ -0,0 +1,21 @@
+﻿using KiracuFlyerAPI.DBContext;
+using KiracuFlyerAPI.Model;
+using KiracuFlyerAPI.Repository.Interface;
+
+namespace KiracuFlyerAPI.Repository
+{
+    public class StatusMasterRepository : IStatusMasterRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public StatusMasterRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<StatusMaster>> GetAllAsync()
+        {
+            return await _context.StatusMasters.ToListAsync();
+        }
+    }
+}
