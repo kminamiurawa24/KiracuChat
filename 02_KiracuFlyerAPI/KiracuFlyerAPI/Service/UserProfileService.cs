e3cad9ed814304d4be3a47fb4afaa7e4d9691c19
kminamiurawa24 <k.minami.urawa024@gmail.com>
2024-12-27 14:20:11 +0900

    サービス層を追加

---
+++ 02_KiracuFlyerAPI/KiracuFlyerAPI/Service/UserProfileService.cs
@@ -0,0 +1,40 @@
+﻿using KiracuFlyerAPI.Model;
+using KiracuFlyerAPI.Repository.Interface;
+using KiracuFlyerAPI.Service.Interface;
+
+namespace KiracuFlyerAPI.Service
+{
+    public class UserProfileService : IUserProfileService
+    {
+        private readonly IUserProfileRepository _userProfileRepository;
+
+        public UserProfileService(IUserProfileRepository userProfileRepository)
+        {
+            _userProfileRepository = userProfileRepository;
+        }
+
+        public int CreateUserProfile(UserProfile userProfile)
+        {
+            // TODO: 必要に応じて、ユーザープロフィールのバリデーションや加工を行う
+
+            return _userProfileRepository.CreateUserProfile(userProfile);
+        }
+
+        public void DeleteUserProfile(int userId)
+        {
+            _userProfileRepository.DeleteUserProfile(userId);
+        }
+
+        public UserProfile GetUserProfile(int userId)
+        {
+            return _userProfileRepository.GetUserProfile(userId);
+        }
+
+        public void UpdateUserProfile(UserProfile userProfile)
+        {
+            // TODO: 必要に応じて、ユーザープロフィールのバリデーションや加工を行う
+
+            _userProfileRepository.UpdateUserProfile(userProfile);
+        }
+    }
+}
---
+++ 02_KiracuFlyerAPI/KiracuFlyerAPI/Service/interface/IUserProfileService.cs
@@ -0,0 +1,19 @@
+﻿using KiracuFlyerAPI.Model;
+
+namespace KiracuFlyerAPI.Service.Interface
+{
+    public interface IUserProfileService
+    {
+        // ユーザープロフィールを作成
+        int CreateUserProfile(UserProfile userProfile);
+
+        // 特定のユーザープロフィールを取得
+        UserProfile GetUserProfile(int userId);
+
+        // 特定のユーザープロフィールを更新
+        void UpdateUserProfile(UserProfile userProfile);
+
+        // 特定のユーザープロフィールを削除
+        void DeleteUserProfile(int userId);
+    }
+}
