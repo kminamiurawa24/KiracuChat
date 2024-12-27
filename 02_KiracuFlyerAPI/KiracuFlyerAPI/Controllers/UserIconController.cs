2809761ca3c5e23ebc77a63650d47ff5bb0a681a
kminamiurawa24 <k.minami.urawa024@gmail.com>
2024-12-27 10:55:23 +0900

    コントローラーを実装

---
+++ 02_KiracuFlyerAPI/KiracuFlyerAPI/Controllers/UserIconController.cs
@@ -0,0 +1,50 @@
+﻿using KiracuFlyerAPI.Request;
+using KiracuFlyerAPI.Service.Interface;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KiracuFlyerAPI.Controllers
+{
+    [ApiController]
+    [Route("user_icons")]
+    public class UserIconController : ControllerBase
+    {
+        private readonly IUserIconService _userIconService;
+
+        public UserIconController(IUserIconService userIconService)
+        {
+            _userIconService = userIconService;
+        }
+
+        [HttpPost]
+        public IActionResult CreateUserIcon([FromBody] CreateUserIconRequest request)
+        {
+            var iconId = _userIconService.CreateUserIcon(request);
+            return CreatedAtAction(nameof(GetUserIcon), new { iconId = iconId }, null);
+        }
+
+        [HttpGet("{iconId}")]
+        public IActionResult GetUserIcon(int iconId)
+        {
+            var userIcon = _userIconService.GetUserIcon(iconId);
+            if (userIcon == null)
+            {
+                return NotFound();
+            }
+            return Ok(userIcon);
+        }
+
+        [HttpPut("{iconId}")]
+        public IActionResult UpdateUserIcon(int iconId, [FromBody] UpdateUserIconRequest request)
+        {
+            _userIconService.UpdateUserIcon(iconId, request);
+            return NoContent();
+        }
+
+        [HttpDelete("{iconId}")]
+        public IActionResult DeleteUserIcon(int iconId)
+        {
+            _userIconService.DeleteUserIcon(iconId);
+            return NoContent();
+        }
+    }
+}
