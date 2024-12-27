2f6f9086e3fb97883c90db3084fd3ecc64780985
kminamiurawa24 <k.minami.urawa024@gmail.com>
2024-12-27 10:53:44 +0900

    サービス層を実装

---
+++ 02_KiracuFlyerAPI/KiracuFlyerAPI/Service/interface/IUserIconService.cs
@@ -0,0 +1,20 @@
+﻿using KiracuFlyerAPI.Model;
+using KiracuFlyerAPI.Request;
+
+namespace KiracuFlyerAPI.Service.Interface
+{
+    public interface IUserIconService
+    {
+        // ユーザーアイコンを作成
+        int CreateUserIcon(CreateUserIconRequest request);
+
+        // 特定のユーザーアイコンを取得
+        UserIcon GetUserIcon(int iconId);
+
+        // 特定のユーザーアイコンを更新
+        void UpdateUserIcon(int iconId, UpdateUserIconRequest request);
+
+        // 特定のユーザーアイコンを削除
+        void DeleteUserIcon(int iconId);
+    }
+}
