f9f5b2b2b2f0bd347de377aa02b3419d7ead242d
kminamiurawa24 <k.minami.urawa024@gmail.com>
2024-12-27 11:19:12 +0900

    コントローラーを実装

---
+++ 02_KiracuFlyerAPI/KiracuFlyerAPI/Controllers/ChannelIconMasterController.cs
@@ -0,0 +1,50 @@
+﻿using KiracuFlyerAPI.Request;
+using KiracuFlyerAPI.Service.Interface;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KiracuFlyerAPI.Controllers
+{
+    [ApiController]
+    [Route("channel_icon_masters")]
+    public class ChannelIconMasterController : ControllerBase
+    {
+        private readonly IChannelIconMasterService _channelIconMasterService;
+
+        public ChannelIconMasterController(IChannelIconMasterService channelIconMasterService)
+        {
+            _channelIconMasterService = channelIconMasterService;
+        }
+
+        [HttpPost]
+        public IActionResult CreateChannelIconMaster([FromBody] CreateChannelIconMasterRequest request)
+        {
+            var iconId = _channelIconMasterService.CreateChannelIconMaster(request);
+            return CreatedAtAction(nameof(GetChannelIconMaster), new { iconId = iconId }, null);
+        }
+
+        [HttpGet("{iconId}")]
+        public IActionResult GetChannelIconMaster(int iconId)
+        {
+            var channelIconMaster = _channelIconMasterService.GetChannelIconMaster(iconId);
+            if (channelIconMaster == null)
+            {
+                return NotFound();
+            }
+            return Ok(channelIconMaster);
+        }
+
+        [HttpPut("{iconId}")]
+        public IActionResult UpdateChannelIconMaster(int iconId, [FromBody] UpdateChannelIconMasterRequest request)
+        {
+            _channelIconMasterService.UpdateChannelIconMaster(iconId, request);
+            return NoContent();
+        }
+
+        [HttpDelete("{iconId}")]
+        public IActionResult DeleteChannelIconMaster(int iconId)
+        {
+            _channelIconMasterService.DeleteChannelIconMaster(iconId);
+            return NoContent();
+        }
+    }
+}
